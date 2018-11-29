13dfcc5e74597fd4e73b98ba109262a243ccee36
KateLe1013 <katele1013@outlook.com>
2018-11-29 15:38:12 +0100

    REFACTOR

--- TDD_BlackJack/src/resource/model/rules/RuleFours.java
+++ TDD_BlackJack/src/resource/model/rules/RuleFours.java
@@ -2,15 +2,17 @@ package resource.model.rules;
 
 public class RuleFours implements IRule {
 
+	private String description;
+
 	@Override
 	public String getDescription() {
-		
-		return "Fours";
+		description = "Fours";
+		return description;
 	}
 
 	@Override
 	public int getScore(int[] dice) {
-		
+
 		return 0;
 	}
 
