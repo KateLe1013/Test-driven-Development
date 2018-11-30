14a7b701bca4ee29cfc86f67ce11b8c24990bbb4
KateLe1013 <katele1013@outlook.com>
2018-11-30 13:58:19 +0100

    Green

--- TDD_BlackJack/src/resource/model/rules/TotalScore.java
+++ TDD_BlackJack/src/resource/model/rules/TotalScore.java
@@ -1,19 +1,24 @@
 package resource.model.rules;
 
-
 public class TotalScore {
 
-	
+	private String rule;
+	private int score;
 	private int totalUpperSectionScore;
 	
 	public TotalScore() {
-		
+
 	}
 
-	
-	
+	public TotalScore(IRule r, int[] dice) {
+		rule = r.getDescription();
+		score = r.getScore(dice);
+	}
+
 	public int getTotalUpperSectionScore() {
-		
+		if (rule.equals("Aces") || rule.equals("Twos") || rule.equals("Threes") || rule.equals("Fours")|| rule.equals("Fives") || rule.equals("Sixes")) {
+			totalUpperSectionScore += score;
+		}
 		return totalUpperSectionScore;
 	}
 }
--- TDD_BlackJack/src/tests/model/TotalScoreTest.java
+++ TDD_BlackJack/src/tests/model/TotalScoreTest.java
@@ -5,14 +5,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
-
-
+import resource.model.rules.IRule;
+import resource.model.rules.RuleTwos;
 import resource.model.rules.TotalScore;
 
 class TotalScoreTest {
 	private TotalScore sut;
-	
+	private IRule r;
+
 	@BeforeEach
 	void setUp() throws Exception {
 		
@@ -24,8 +26,14 @@ class TotalScoreTest {
 
 	@Test
 	void shouldReturnTotalUpperSectionScore() {
-		sut=new TotalScore();
+		r=mock(RuleTwos.class);
+		int []dice= {1,2,3,2,2};
+		when(r.getDescription()).thenReturn(RuleTwos.class.getSimpleName().substring(4));
+		when(r.getScore(dice)).thenReturn(6);
+		sut=new TotalScore(r,dice);
 		assertEquals(6, sut.getTotalUpperSectionScore());
+		verify(r,times(1)).getDescription();
+		verify(r,times(1)).getScore(dice);
 	}
 
 }
