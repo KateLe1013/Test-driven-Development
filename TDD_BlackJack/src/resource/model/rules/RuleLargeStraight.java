2006f8f68fd6317fd98bba990be8fb429429b88e
KateLe1013 <katele1013@outlook.com>
2018-11-30 10:21:56 +0100

    REFACTOR

--- TDD_BlackJack/src/resource/model/rules/RuleLargeStraight.java
+++ TDD_BlackJack/src/resource/model/rules/RuleLargeStraight.java
@@ -2,15 +2,17 @@ package resource.model.rules;
 
 public class RuleLargeStraight implements IRule {
 
+	private String description;
+
 	@Override
 	public String getDescription() {
-		
-		return "Large Straight";
+		description = "Large Straight";
+		return description;
 	}
 
 	@Override
 	public int getScore(int[] dice) {
-		
+
 		return 0;
 	}
 
