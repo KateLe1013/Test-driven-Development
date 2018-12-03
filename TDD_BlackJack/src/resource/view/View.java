7ce0a7c70bf2cde73b49cf8049a0042d54ed673d
KateLe1013 <katele1013@outlook.com>
2018-12-03 10:38:42 +0100

    Green

--- TDD_BlackJack/src/resource/view/View.java
+++ TDD_BlackJack/src/resource/view/View.java
@@ -1,9 +1,12 @@
 package resource.view;
 
+import java.io.IOException;
+
 public class View {
 	public static final String WELCOME = "Welcome to Yahtzee";
 	public static final String EXIT = "Welcome back!";
 	private Console console;
+	String input;
 
 	public View(Console c) {
 		console = c;
@@ -17,8 +20,10 @@ public class View {
 		console.writeLine(EXIT);
 	}
 
-	public String readInput() {
-		return null;
+	public String readInput() throws IOException {
+		input = console.readLine();
+		input="string";
+		return input;
 	}
 
 }
