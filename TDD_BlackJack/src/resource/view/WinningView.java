a23a78fb2b7342787c209971ca2f16631e050252
KateLe1013 <katele1013@outlook.com>
2018-12-04 13:47:30 +0100

    GREEN

--- TDD_BlackJack/src/resource/view/WinningView.java
+++ TDD_BlackJack/src/resource/view/WinningView.java
@@ -7,6 +7,7 @@ import resource.model.rules.IRule;
 public class WinningView {
 	private IRule rule;
 	private View view;
+	private int choice;
 
 	public WinningView(IRule r, View v) {
 		rule = r;
@@ -14,6 +15,46 @@ public class WinningView {
 	}
 
 	public void checkWinningRule(int[] win) throws IOException {
-		throw new IOException();
+		System.out.println("Which do you want to see if you have?");
+		if (win[0] == 0) {
+			System.out.println("1 - Yathzee");
+		}
+		if (win[1] == 0) {
+			System.out.println("2 - Full House");
+		}
+		if (win[2] == 0) {
+			System.out.println("3 - Large Straigt");
+		}
+		if (win[3] == 0) {
+			System.out.println("4 - Small Straigt");
+		}
+		if (win[4] == 0) {
+			System.out.println("5 - Four Of A Kind");
+		}
+		if (win[5] == 0) {
+			System.out.println("6 - Three Of A Kind");
+		}
+		if (win[6] == 0) {
+			System.out.println("7 - Chance");
+		}
+		if (win[7] == 0) {
+			System.out.println("8 - 1's");
+		}
+		if (win[8] == 0) {
+			System.out.println("9 - 2's");
+		}
+		if (win[9] == 0) {
+			System.out.println("10 - 3's");
+		}
+		if (win[10] == 0) {
+			System.out.println("11 - 4's");
+		}
+		if (win[11] == 0) {
+			System.out.println("12 - 5's");
+		}
+		if (win[12] == 0) {
+			System.out.println("13 - 6's");
+		}
+		choice = view.readInputInt("");
 	}
 }
